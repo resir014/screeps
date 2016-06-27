13bafb989a0a84180e279efb8d26cf52d50c41f6
Resi Respati <resir014@gmail.com>
2016-06-27 23:35:30 +0700

    [Harvester] Added sanity check for setTargetDropOff

--- src/components/creeps/harvester.ts
+++ src/components/creeps/harvester.ts
@@ -62,7 +62,9 @@ export class Harvester extends CreepAction implements IHarvester, ICreepAction {
       }
     });
 
-    this.creep.memory.target_energy_dropoff_id = targets[0].id;
+    if (targets.length > 0) {
+      this.creep.memory.target_energy_dropoff_id = targets[0].id;
+    }
   }
 
   public action(): boolean {
