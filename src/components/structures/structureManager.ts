80fb0820d5c27636927a1d4ed1195ee0bd1b1dc3
Resi Respati <resir014@gmail.com>
2016-07-02 16:42:11 +0700

    [StructureManager] Return to find() calls
    TODO: find() calls are much more expensive, let's try to find() once and cache the result

--- src/components/structures/structureManager.ts
+++ src/components/structures/structureManager.ts
@@ -19,42 +19,56 @@ export namespace StructureManager {
     return this.structures[0];
   }
 
+  // TODO find() calls are much more expensive, let's try to find() once and
+  // cache the result
   export function getStorageObject(): Structure {
-    let targets: Structure[] = _.filter(this.structures, (structure: StructureContainer | StructureStorage) => {
-      return ((structure.structureType == STRUCTURE_CONTAINER)
-        && _.sum(structure.store) < structure.storeCapacity);
+    let targets: Structure[] = <Structure[]>RoomManager.getFirstRoom().find(FIND_STRUCTURES, {
+      filter: (structure: StructureContainer | StructureStorage) => {
+        return ((structure.structureType == STRUCTURE_CONTAINER)
+          && _.sum(structure.store) < structure.storeCapacity);
+      }
     });
 
     // if we can't find any storage containers, use either the extension or spawn.
     if (targets.length == 0) {
-      targets = _.filter(this.structures, (structure: StructureExtension) => {
-        return ((structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-          structure.energy < structure.energyCapacity);
+      targets = <Structure[]>RoomManager.getFirstRoom().find(FIND_STRUCTURES, {
+        filter: (structure: StructureExtension) => {
+          return ((structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+            structure.energy < structure.energyCapacity);
+        }
       });
     }
 
     return targets[0];
   }
 
+  // TODO find() calls are much more expensive, let's try to find() once and
+  // cache the result
   export function getDropOffPoint(): Structure {
-    let targets: Structure[] = _.filter(this.structures, (structure: Spawn) => {
-      return ((structure.structureType == STRUCTURE_SPAWN)
-        && structure.energy < structure.energyCapacity);
+    let targets: Structure[] = <Structure[]>RoomManager.getFirstRoom().find(FIND_STRUCTURES, {
+      filter: (structure: Spawn) => {
+        return ((structure.structureType == STRUCTURE_SPAWN)
+          && structure.energy < structure.energyCapacity);
+      }
     });
 
     // if we can't find any storage containers, use the extension.
     if (targets.length == 0) {
-      targets = _.filter(this.structures, (structure: StructureExtension) => {
-        return ((structure.structureType == STRUCTURE_EXTENSION) &&
-          structure.energy < structure.energyCapacity);
+      targets = <Structure[]>RoomManager.getFirstRoom().find(FIND_STRUCTURES, {
+        filter: (structure: StructureExtension) => {
+          return ((structure.structureType == STRUCTURE_EXTENSION) &&
+            structure.energy < structure.energyCapacity);
+        }
       });
     }
 
     // Same thing, but we now look for storage.
     if (targets.length == 0) {
-      targets = _.filter(this.structures, (structure: StructureContainer | StructureStorage) => {
-        return ((structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE) &&
-          _.sum(structure.store) < structure.storeCapacity);
+      targets = <Structure[]>RoomManager.getFirstRoom().find(FIND_STRUCTURES, {
+        filter: (structure: StructureContainer | StructureStorage) => {
+          return ((structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE) &&
+            _.sum(structure.store) < structure.storeCapacity);
+        }
       });
     }
 
