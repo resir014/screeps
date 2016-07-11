f6a89405de9f21d5b94a837cc71069385afdb0df
Resi Respati <resir014@gmail.com>
2016-07-11 14:53:22 +0700

    [StructureManager] Fixed some filters
    Also changed FIND_MY_STRUCTURES to FIND_STRUCTURES

--- src/components/structures/structureManager.ts
+++ src/components/structures/structureManager.ts
@@ -7,7 +7,7 @@ export namespace StructureManager {
   export var structureCount: number = 0;
 
   export function loadStructures(room: Room) {
-    structures = room.find<Structure>(FIND_MY_STRUCTURES);
+    structures = room.find<Structure>(FIND_STRUCTURES);
     structureCount = _.size(structures);
 
     if (Config.VERBOSE) {
@@ -22,14 +22,14 @@ export namespace StructureManager {
   // TODO find() calls are much more expensive, let's try to find() once and
   // cache the result
   export function getStorageObject(): Structure {
-    let targets: Structure[] = _.filter(structures, (structure: StructureContainer) => {
+    let targets: Structure[] = structures.filter((structure: StructureContainer) => {
       return ((structure.structureType == STRUCTURE_CONTAINER)
         && _.sum(structure.store) < structure.storeCapacity);
     });
 
     // if we can't find any storage containers, use either the extension or spawn.
     if (targets.length == 0) {
-      targets = _.filter(structures, (structure: StructureExtension) => {
+      targets = structures.filter((structure: StructureExtension) => {
         return ((structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
           structure.energy < structure.energyCapacity);
       });
@@ -41,7 +41,7 @@ export namespace StructureManager {
   // TODO find() calls are much more expensive, let's try to find() once and
   // cache the result
   export function getDropOffPoint(): Structure {
-    let targets: Structure[] = _.filter(structures, (structure) => {
+    let targets: Structure[] = structures.filter((structure) => {
       if (structure instanceof Spawn) {
         return ((structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity);
       }
@@ -49,7 +49,7 @@ export namespace StructureManager {
 
     // If the spawn is full, we'll find any extensions/towers.
     if (targets.length == 0) {
-      targets = _.filter(structures, (structure) => {
+      targets = structures.filter((structure) => {
         if (structure instanceof StructureExtension) {
           return ((structure.structureType == STRUCTURE_EXTENSION)
             && structure.energy < structure.energyCapacity);
@@ -59,7 +59,7 @@ export namespace StructureManager {
 
     // Or if that's filled as well, look for towers.
     if (targets.length == 0) {
-      targets = _.filter(structures, (structure: StructureTower) => {
+      targets = structures.filter((structure: StructureTower) => {
         return ((structure.structureType == STRUCTURE_TOWER)
           && structure.energy < structure.energyCapacity - (structure.energyCapacity * 0.5));
       });
@@ -67,37 +67,43 @@ export namespace StructureManager {
 
     // Otherwise, look for storage containers.
     if (targets.length == 0) {
-      targets = _.filter(structures, (structure: StructureStorage) => {
+      targets = structures.filter((structure: StructureStorage) => {
         return ((structure.structureType == STRUCTURE_STORAGE) && _.sum(structure.store) < structure.storeCapacity);
       });
     }
-
     return targets[0];
   }
 
   // TODO find() calls are much more expensive, let's try to find() once and
   // cache the result
   export function getStructuresToRepair(): Structure {
-    let targets: Structure[] = _.filter(structures, (structure: Structure) => {
+    let targets: Structure[] = structures.filter((structure: Structure) => {
       return ((structure.hits < (structure.hitsMax - (structure.hitsMax * 0.3))
-        && (structure.structureType !== STRUCTURE_WALL && structure.structureType !== STRUCTURE_RAMPART
-        && structure.structureType !== STRUCTURE_CONTROLLER)));
+        && (structure.structureType !== STRUCTURE_WALL && structure.structureType !== STRUCTURE_ROAD
+        && structure.structureType !== STRUCTURE_RAMPART)));
     });
+
+    if (targets.length == 0) {
+      targets = structures.filter((structure: Structure) => {
+        return ((structure.hits < (structure.hitsMax - (structure.hitsMax * 0.3))
+          && (structure.structureType !== STRUCTURE_WALL && structure.structureType !== STRUCTURE_RAMPART)));
+      })
+    }
+
+    if (targets.length == 0) {
+      targets = structures.filter((structure: Structure) => {
+        return ((structure.hits < (structure.hitsMax - (structure.hitsMax * 0.3))
+          && (structure.structureType !== STRUCTURE_WALL)));
+      })
+    }
 
     return targets[0];
   }
 
   export function getDefensiveStructuresToRepair(): Structure {
-    let targets: Structure[] = _.filter(structures, (structure: Structure) => {
-      return ((structure.hits < (structure.hitsMax - (structure.hitsMax * 0.5))
-        && (structure.structureType === STRUCTURE_RAMPART)));
+    let targets: Structure[] = structures.filter((structure: Structure) => {
+      return (structure.structureType === STRUCTURE_WALL && structure.hits < Config.MIN_WALL_HEALTH);
     });
-
-    if (targets.length == 0) {
-      targets = _.filter(this.structures, (structure: Structure) => {
-        return (structure.structureType === STRUCTURE_WALL && structure.hits < Config.MIN_WALL_HEALTH);
-      })
-    }
 
     return targets[0];
   }
