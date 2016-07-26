ab99e4798f241e6d18cc4a809e3fccd3d787c406
Resi Respati <resir014@gmail.com>
2016-07-26 16:36:03 +0700

    removed some unnecessary config vars

--- src/config/config.ts
+++ src/config/config.ts
@@ -11,21 +11,6 @@ export const VERBOSE: boolean = false;
 export const MAX_HARVESTERS_PER_SOURCE: number = 2;
 
 /**
- * @type {number}
- */
-export const MAX_ENERGY_REFILL_THRESHOLD: number = 10;
-
-/**
- * @type {number}
- */
-export const MIN_WALL_HEALTH: number = 200000;
-
-/**
- * @type {number}
- */
-export const MIN_RAMPART_HEALTH: number = 150000;
-
-/**
  * Default amount of minimal ticksToLive a creep can have, before it goes
  * to renew. This is only default value, that don't have to be used.
  * So it doesn't cover all Screeps.
@@ -33,11 +18,3 @@ export const MIN_RAMPART_HEALTH: number = 150000;
  * @type {number}
  */
 export const DEFAULT_MIN_LIFE_BEFORE_NEEDS_REFILL: number = 400;
-
-/**
- * Default amount of minimal hit points a Structure can have before it needs
- * to be repaired.
- *
- * @type {number}
- */
-export const DEFAULT_MIN_HITS_BEFORE_NEEDS_REPAIR: number = 600;
