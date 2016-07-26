a5c3d9badeeef0c6343c16a8c7e0b1e7c91a2085
Resi Respati <resir014@gmail.com>
2016-07-26 15:55:35 +0700

    Refactors tryRetrieveEnergy into CreepAction

--- src/components/creeps/creepAction.ts
+++ src/components/creeps/creepAction.ts
@@ -14,6 +14,7 @@ interface ICreepAction {
   needsRenew(): boolean;
   tryRenew(spawn: Spawn): number;
   moveToRenew(spawn: Spawn): void;
+  tryRetrieveEnergy(): void;
 }
 
 /**
@@ -77,6 +78,44 @@ export class CreepAction {
   public moveToRenew(spawn: Spawn): void {
     if (this.tryRenew(spawn) === ERR_NOT_IN_RANGE) {
       this.creep.moveTo(spawn);
+    }
+  }
+
+  /**
+   * Retrieves energy from any dropped resources or containers.
+   *
+   * Creeps will attempt to retrieve any dropped energy within the room, if
+   * nothing is found, then they will look for any containers which hold more
+   * than 500 units of energy.
+   */
+  public tryRetrieveEnergy(): void {
+    let targetSource = this.creep.pos.findClosestByPath<Resource>(FIND_DROPPED_RESOURCES);
+
+    if (targetSource) {
+      if (this.creep.pos.isNearTo(targetSource)) {
+        this.creep.pickup(targetSource);
+      } else {
+        this.moveTo(targetSource);
+      }
+    } else {
+      let targetContainer = this.creep.pos.findClosestByPath<Container>(FIND_STRUCTURES, {
+        filter: ((structure: Structure) => {
+          if (structure.structureType === STRUCTURE_CONTAINER) {
+            let container = <Container>structure;
+            if (_.sum(container.store) > (500)) {
+              return container;
+            }
+          }
+        })
+      });
+
+      if (targetContainer) {
+        if (this.creep.pos.isNearTo(targetContainer)) {
+          this.creep.withdraw(targetContainer, RESOURCE_ENERGY);
+        } else {
+          this.moveTo(targetContainer);
+        }
+      }
     }
   }
 
--- src/components/creeps/roles/builder.ts
+++ src/components/creeps/roles/builder.ts
@@ -49,34 +49,7 @@ export class Builder extends CreepAction {
       }
 
     } else {
-      let targetSource = this.creep.pos.findClosestByPath<Resource>(FIND_DROPPED_RESOURCES);
-
-      if (targetSource) {
-        if (this.creep.pos.isNearTo(targetSource)) {
-          this.creep.pickup(targetSource);
-        } else {
-          this.moveTo(targetSource);
-        }
-      } else {
-        let targetContainer = this.creep.pos.findClosestByPath<Container>(FIND_STRUCTURES, {
-          filter: ((structure: Structure) => {
-            if (structure.structureType === STRUCTURE_CONTAINER) {
-              let container = <Container>structure;
-              if (_.sum(container.store) > (500)) {
-                return container;
-              }
-            }
-          })
-        });
-
-        if (targetContainer) {
-          if (this.creep.pos.isNearTo(targetContainer)) {
-            this.creep.withdraw(targetContainer, RESOURCE_ENERGY);
-          } else {
-            this.moveTo(targetContainer);
-          }
-        }
-      }
+      this.tryRetrieveEnergy();
     }
   }
 
