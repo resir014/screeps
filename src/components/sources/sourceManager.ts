ca5cb55bbeed3bb4166fbddda453612666736311
Resi Respati <resi.respati@c88fin.com>
2017-08-23 18:50:13 +0700

    [SourceManager] don't update job entries outside of init tick

--- src/components/sources/sourceManager.ts
+++ src/components/sources/sourceManager.ts
@@ -26,8 +26,6 @@ export function refreshAvailableSources(room: Room): void {
     room.memory.sources = _.filter((room.memory.sources as string[]), (id: string) => {
       return _.includes(blacklistedSources, id) === false
     })
-
-    room.memory.jobs.harvester = room.memory.sources.length
   }
 
   if (ENABLE_DEBUG_MODE) {
