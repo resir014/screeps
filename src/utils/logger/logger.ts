0d17bf961dedba6c704768e3a25985b37ce339ab
Resi Respati <resir014@gmail.com>
2017-08-24 23:47:43 +0700

    refactor: moved logger from class to namespace

--- src/utils/logger/logger.ts
+++ src/utils/logger/logger.ts
@@ -2,50 +2,50 @@ import * as Config from '../../config/config'
 import * as Inscribe from '../../lib/Inscribe'
 import { LogLevel } from './logLevel'
 
-export class Logger {
-  public static error(message: (() => string) | string) {
+export namespace Logger {
+  export function error(message: (() => string) | string) {
     write(LogLevel.ERROR, message)
   }
 
-  public static warn(message: (() => string) | string) {
+  export function warn(message: (() => string) | string) {
     write(LogLevel.WARNING, message)
   }
 
-  public static info(message: (() => string) | string) {
+  export function info(message: (() => string) | string) {
     write(LogLevel.INFO, message)
   }
 
-  public static debug(message: (() => string) | string) {
+  export function debug(message: (() => string) | string) {
     write(LogLevel.DEBUG, message)
   }
-}
 
-function write(level: LogLevel, message: (() => string) | string): void {
-  const out: string[] = []
-  switch (level) {
-    case LogLevel.DEBUG: {
-      out.push(Inscribe.color(_.padRight('DEBUG', 8, ' '), 'gray'))
-      break
+  function write(level: LogLevel, message: (() => string) | string): void {
+    const out: string[] = []
+    switch (level) {
+      case LogLevel.DEBUG: {
+        out.push(Inscribe.color(_.padRight('DEBUG', 8, ' '), 'gray'))
+        break
+      }
+      case LogLevel.INFO: {
+        out.push(Inscribe.color(_.padRight('INFO', 8, ' '), 'lime'))
+        break
+      }
+      case LogLevel.WARNING: {
+        out.push(Inscribe.color(_.padRight('WARNING', 8, ' '), 'yellow'))
+        break
+      }
+      case LogLevel.ERROR: {
+        out.push(Inscribe.color(_.padRight('ERROR', 8, ' '), 'red'))
+        break
+      }
+      default: {
+        break
+      }
     }
-    case LogLevel.INFO: {
-      out.push(Inscribe.color(_.padRight('INFO', 8, ' '), 'lime'))
-      break
+    if (Config.LOG_PRINT_TICK) {
+      out.push(Inscribe.time())
     }
-    case LogLevel.WARNING: {
-      out.push(Inscribe.color(_.padRight('WARNING', 8, ' '), 'yellow'))
-      break
-    }
-    case LogLevel.ERROR: {
-      out.push(Inscribe.color(_.padRight('ERROR', 8, ' '), 'red'))
-      break
-    }
-    default: {
-      break
-    }
-  }
-  if (Config.LOG_PRINT_TICK) {
-    out.push(Inscribe.time())
-  }
 
-  console.log(out.join(' '), message)
+    console.log(out.join(' '), message)
+  }
 }
