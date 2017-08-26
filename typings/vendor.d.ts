810403c7a8bd61abd5075ea25fe368bdb8740718
Resi Respati <resir014@gmail.com>
2017-08-26 22:23:16 +0700

    remove no longer needed type shims

--- typings/vendor.d.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-// Put shims and extensions to installed modules and typings here
-
-// shim uglify-js for webpack
-declare module "uglify-js" {
-  export interface MinifyOptions {}
-}
