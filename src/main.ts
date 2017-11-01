b0facc224866eb7b5ef5fbf596751d9f8ca20c9b
Resi Respati <resi.respati@compare88.net>
2017-11-01 17:48:13 +0700

    Refactored roomManager back into main

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import * as Inscribe from 'screeps-inscribe'
 import * as Config from './config/config'
 import { Orchestrator } from './utils/orchestrator'
 import { checkOutOfBoundsMemory } from './shared/memoryManager'
-import { initialiseRooms } from './shared/roomManager'
+
+import { runCreeps } from './components/creeps/creepManager'
+import { refreshAvailableSources } from './components/sources/sourceManager'
+import { runTowers } from './components/towers/towerManager'
+import { refreshJobAssignments } from './shared/jobManager'
+import {
+  initialiseRoomMemory,
+  refreshMiningPositions,
+  cleanupCreepMemory
+} from './shared/memoryManager'
 
 import { Logger } from './utils/logger'
 import { loadCreepPrototypes } from './prototypes/Creep'
@@ -44,5 +53,18 @@ export function loop(): void {
   checkOutOfBoundsMemory()
 
   // Initialise all controlled rooms.
-  initialiseRooms()
+  _.each(Game.rooms, (room: Room) => {
+    // Memory cleanup tasks
+    initialiseRoomMemory(room)
+    refreshMiningPositions(room)
+    cleanupCreepMemory(room)
+    refreshJobAssignments(room)
+
+    // Component initialisation tasks
+    refreshAvailableSources(room)
+
+    // For each tick, run managed creeps/structures
+    runCreeps(room)
+    runTowers(room)
+  })
 }
