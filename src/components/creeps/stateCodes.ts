bfb360188d0dda38502824734132518679a07e2a
Resi Respati <resir014@gmail.com>
2017-11-25 12:45:56 +0700

    nuke old codebase

--- src/components/creeps/stateCodes.ts
+++
@@ -1,5 +0,0 @@
-export const STATE_SPAWNING = 0
-export const STATE_MOVING = 1
-export const STATE_HARVESTING = 2
-export const STATE_DEPOSIT_RESOURCE = 3
-export const STATE_GRAB_RESOURCE = 4
--- src/utils/logger/logLevel.ts
+++
@@ -1,6 +0,0 @@
-export enum LogLevel {
-  ERROR,
-  WARNING,
-  INFO,
-  DEBUG
-}
