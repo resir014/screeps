812876b1acad938863a6a2bee30f2243b450e833
Resi Respati <resir014@gmail.com>
2017-11-27 00:11:51 +0700

    minor housekeeping

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from 'lib/Logger'
+import { Logger, LogLevel } from 'lib/Logger'
 import { BaseKernel } from 'os/BaseKernel'
 import { ProcessRegistry } from 'os/ProcessRegistry'
 import { ExtensionRegistry } from 'os/ExtensionRegistry'
@@ -6,6 +6,14 @@ import { ExtensionRegistry } from 'os/ExtensionRegistry'
 import { bundle as bin } from 'bin'
 
 import './globals'
+
+const log = new Logger('[main]')
+log.level = LogLevel.DEBUG
+
+log.info('Stonehenge (v0.0.3)')
+log.info('Smart colony management for the game Screeps. Written in TypeScript.')
+
+log.debug('Bootstrapping...')
 
 export const processRegistry = new ProcessRegistry()
 export const extensionRegistry = new ExtensionRegistry()
@@ -16,6 +24,8 @@ global.kernel = kernel
 extensionRegistry.register('BaseKernel', kernel)
 
 processRegistry.install(bin)
+
+log.debug('Kernel bootstrapped.')
 
 export function loop() {
   kernel.start()
