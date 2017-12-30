1470e576b931182e89ee15b1a587af64e4e97ae2
Resi Respati <resir014@gmail.com>
2017-12-30 14:21:43 +0700

    use environment variables on rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,13 @@
 "use strict";
 
 import clean from "rollup-plugin-clean";
+import replace from "rollup-plugin-replace";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import screeps from "rollup-plugin-screeps";
 
-let cfg;
-const i = process.argv.indexOf("--dest") + 1;
-if (i == 0) {
-  console.log("No destination specified - code will be compiled but not uploaded");
-} else if (i >= process.argv.length || (cfg = require("./screeps")[process.argv[i]]) == null) {
-  throw new Error("Invalid upload destination");
-}
+const cfg = process.env.NODE_ENV === 'production' ? 'main' : 'sim';
 
 export default {
   input: "src/main.ts",
@@ -25,9 +20,19 @@ export default {
 
   plugins: [
     clean(),
+    replace({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      },
+      __BUILD_TIME__: JSON.stringify(Date.now())
+    }),
     resolve(),
     commonjs(),
     typescript({tsconfig: "./tsconfig.json"}),
-    screeps({config: cfg, dryRun: cfg == null})
+    screeps({
+      config: require("./screeps")[cfg],
+      // if `NODE_ENV` is local, perform a dry run
+      dryRun: process.env.NODE_ENV === 'local'
+    })
   ]
 }
